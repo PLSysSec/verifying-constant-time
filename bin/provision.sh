40a9adfe29523a02b78f7befc425506d1169b8fb
root <root@second.lxd>
2017-09-12 22:46:36 +0000

    bumped up tools' versions

--- bin/provision.sh
+++ bin/provision.sh
@@ -33,19 +33,19 @@ CMAKE_INSTALL_PREFIX=
 DEPENDENCIES="cmake python-yaml unzip wget"
 
 Z3_DOWNLOAD_LINK="https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/z3-4.4.1-x64-ubuntu-14.04.zip"
-DEPENDENCIES+=" clang-3.5 llvm-3.5 mono-complete libz-dev libedit-dev"
+DEPENDENCIES+=" clang-3.8 llvm-3.8 mono-complete libz-dev libedit-dev"
 DEPENDENCIES+=" ruby2.2"
 
 # Install core dependencies from packages
-sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main"
+#sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main"
 ${WGET} -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo add-apt-repository ppa:brightbox/ruby-ng-experimental # for ruby2.2
 sudo apt-get update
 sudo apt-get install -y ${DEPENDENCIES}
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.5 20
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 20
-sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.5 20
-sudo update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-3.5 20
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 20
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 20
+sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.8 20
+sudo update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-3.8 20
 
 # Clone tools' and examples' projects
 git submodule init
